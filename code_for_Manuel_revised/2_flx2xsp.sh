7a94ead51534cba413dcb8dab36cee4c0763b758
mcrnogor <mcrnogor@astro.umd.edu>
2020-08-10 22:52:25 -0400

    Update 2_flx2xsp.sh

--- code_for_Manuel_revised/2_flx2xsp.sh
+++ code_for_Manuel_revised/2_flx2xsp.sh
@@ -4,6 +4,6 @@ for i in {0..29}
 
 do
 
-flx2xsp bn121225417_ALP_xspec_$i.txt bn121225417_ALP_xspec_$i.pha bn121225417_ALP_xspec_$i.rsp xunit=keV yunit=ph/cm^2/s/keV
+flx2xsp bn121225417_ALP_xspec_$i.txt bn121225417_ALP_xspec_$i.pha bn121225417_ALP_xspec_$i.rsp xunit=MeV yunit=ph/cm^2/s/MeV
 
 done
